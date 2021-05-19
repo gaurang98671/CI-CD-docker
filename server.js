cf4b067a575d1bc1d0196ca7d125cffd43ede651
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-05-19 19:51:59 +0530

    testing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res)=>{
-    res.status(200).send("Hello")
+    res.status(200).send("Hello world")
 })
 
 app.listen(8080, ()=>{
